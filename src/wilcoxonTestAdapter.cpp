438e4a3c1b052303b9b4130d1823ff5fbc076d06
stela2502 <Stefan.Lang@med.lu.se>
2019-02-25 21:01:23 +0100

    Wilcox test captured from https://github.com/stenver/wilcoxon-test

---
+++ src/wilcoxonTestAdapter.cpp
@@ -0,0 +1,52 @@
+/* source copied from https://github.com/stenver/wilcoxon-test*/
+
+#include <WilcoxonTest.h>
+
+#include <gsl/gsl_cdf.h>
+#include <Rcpp.h>
+
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+
+RcppExport SEXP wilcoxonTestAdapter(
+  SEXP dataMatrix,
+  SEXP testIndexes,
+  SEXP controlIndexes) {
+BEGIN_RCPP
+  try{
+    Rcpp::NumericMatrix rccpData(dataMatrix);
+    Rcpp::NumericVector rccpTestIndexes(testIndexes);
+    Rcpp::NumericVector rccpControlIndexes(controlIndexes);
+    
+    std::vector<float> transposedData;
+    for(unsigned int i = 0; i < rccpData.nrow(); i++){
+      for(unsigned int j = 0; j < rccpData.ncol(); j++){
+        transposedData.push_back(rccpData(i, j));
+      }
+    }
+    
+    float* _data = &transposedData[0];
+    int _dataXsize = rccpData.ncol();
+    int _dataYsize = rccpData.nrow();
+    std::vector<int> _testIndexes = Rcpp::as<std::vector<int> >(rccpTestIndexes);
+    std::vector<int> _controlIndexes = Rcpp::as<std::vector<int> >(rccpControlIndexes);
+
+    WilcoxonTest wilx(_data, _dataXsize, _dataYsize, &_testIndexes, &_controlIndexes);
+
+    std::vector<double> * pValues = wilx.test();
+
+    Rcpp::NumericVector results( pValues->begin(), pValues->end() );
+
+    return results;
+
+  }catch(exception& e)
+  {
+    std::cout << "Error" << std::endl;
+    std::cout << e.what() << std::endl;
+    return R_NilValue;
+  }
+END_RCPP
+}
