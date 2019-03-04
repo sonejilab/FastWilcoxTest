2c7abdddd3700dfe525aacf67f805a40a189cd63
stela2502 <Stefan.Lang@med.lu.se>
2019-03-04 12:49:00 +0100

    Passing!!

--- ForceCrash.R
+++ ForceCrash.R
@@ -1,9 +1,13 @@
+## R -d "valgrind --dsymutil=yes" 
+
 devtools::load_all()
 load('X.RData') # a big sparse matrix
 
-system.time({ Cpp = StatTest(Matrix::t(X), 1:20, 30:80, .1) })
+Cpp = StatTest(Matrix::t(X), 1:20, 30:80, .1) 
 
-system.time({ Cpp = StatTest(Matrix::t(X), 1:20, 30:80, .1) })
+Cpp = StatTest(Matrix::t(X), 1:20, 30:80, .1)
+
+## you will likely not reach this ;-)
 
 system.time({ Cpp = StatTest(Matrix::t(X), 1:20, 30:80, .1) })
 
