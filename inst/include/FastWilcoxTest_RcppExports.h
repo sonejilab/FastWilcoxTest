d52b6313b992b609d8bacb409a5db8363036ddc8
Stefan Lang <Stefan.Lang@med.lu.se>
2019-03-05 16:19:52 +0100

    Added a fastWilcoxTest(x,y, type) function that works like wilcox.test()

--- inst/include/FastWilcoxTest_RcppExports.h
+++ inst/include/FastWilcoxTest_RcppExports.h
@@ -46,17 +46,38 @@ namespace FastWilcoxTest {
         return Rcpp::as<double >(rcpp_result_gen);
     }
 
-    inline NumericMatrix StatTest(Eigen::MappedSparseMatrix<double> X, std::vector<int> interest, std::vector<int> background, double logFCcut = 1.0, double minPct = 0.1) {
-        typedef SEXP(*Ptr_StatTest)(SEXP,SEXP,SEXP,SEXP,SEXP);
+    inline std::vector<double> fastWilcoxTest(std::vector<double> x, std::vector<double> y, int type) {
+        typedef SEXP(*Ptr_fastWilcoxTest)(SEXP,SEXP,SEXP);
+        static Ptr_fastWilcoxTest p_fastWilcoxTest = NULL;
+        if (p_fastWilcoxTest == NULL) {
+            validateSignature("std::vector<double>(*fastWilcoxTest)(std::vector<double>,std::vector<double>,int)");
+            p_fastWilcoxTest = (Ptr_fastWilcoxTest)R_GetCCallable("FastWilcoxTest", "_FastWilcoxTest_fastWilcoxTest");
+        }
+        RObject rcpp_result_gen;
+        {
+            RNGScope RCPP_rngScope_gen;
+            rcpp_result_gen = p_fastWilcoxTest(Shield<SEXP>(Rcpp::wrap(x)), Shield<SEXP>(Rcpp::wrap(y)), Shield<SEXP>(Rcpp::wrap(type)));
+        }
+        if (rcpp_result_gen.inherits("interrupted-error"))
+            throw Rcpp::internal::InterruptedException();
+        if (Rcpp::internal::isLongjumpSentinel(rcpp_result_gen))
+            throw Rcpp::LongjumpException(rcpp_result_gen);
+        if (rcpp_result_gen.inherits("try-error"))
+            throw Rcpp::exception(Rcpp::as<std::string>(rcpp_result_gen).c_str());
+        return Rcpp::as<std::vector<double> >(rcpp_result_gen);
+    }
+
+    inline NumericMatrix StatTest(Eigen::MappedSparseMatrix<double> X, std::vector<int> interest, std::vector<int> background, double logFCcut = 1.0, double minPct = 0.1, bool onlyPos = false) {
+        typedef SEXP(*Ptr_StatTest)(SEXP,SEXP,SEXP,SEXP,SEXP,SEXP);
         static Ptr_StatTest p_StatTest = NULL;
         if (p_StatTest == NULL) {
-            validateSignature("NumericMatrix(*StatTest)(Eigen::MappedSparseMatrix<double>,std::vector<int>,std::vector<int>,double,double)");
+            validateSignature("NumericMatrix(*StatTest)(Eigen::MappedSparseMatrix<double>,std::vector<int>,std::vector<int>,double,double,bool)");
             p_StatTest = (Ptr_StatTest)R_GetCCallable("FastWilcoxTest", "_FastWilcoxTest_StatTest");
         }
         RObject rcpp_result_gen;
         {
             RNGScope RCPP_rngScope_gen;
-            rcpp_result_gen = p_StatTest(Shield<SEXP>(Rcpp::wrap(X)), Shield<SEXP>(Rcpp::wrap(interest)), Shield<SEXP>(Rcpp::wrap(background)), Shield<SEXP>(Rcpp::wrap(logFCcut)), Shield<SEXP>(Rcpp::wrap(minPct)));
+            rcpp_result_gen = p_StatTest(Shield<SEXP>(Rcpp::wrap(X)), Shield<SEXP>(Rcpp::wrap(interest)), Shield<SEXP>(Rcpp::wrap(background)), Shield<SEXP>(Rcpp::wrap(logFCcut)), Shield<SEXP>(Rcpp::wrap(minPct)), Shield<SEXP>(Rcpp::wrap(onlyPos)));
         }
         if (rcpp_result_gen.inherits("interrupted-error"))
             throw Rcpp::internal::InterruptedException();
