1979ba99531d59987a6f540743270e48e9176c9a
Stefan Lang <Stefan.Lang@med.lu.se>
2019-03-06 16:21:01 +0100

    might still be faster for very sparse matrices like a single cell expression data set.

--- tests/testthat/test-class.R
+++ tests/testthat/test-class.R
@@ -3,7 +3,7 @@ context( 'Class usage')
 set.seed(1)
 ncol = 100
 nrow=90
-dat = matrix(round(rnorm(ncol*nrow,mean = 1, sd = 1)),ncol=ncol)
+dat = matrix(round(rnorm(ncol*nrow,mean = 0, sd = 1)),ncol=ncol)
 dat[which(dat < 1)] = 0
 colnames(dat) <- paste('Sample', 1:ncol)
 rownames(dat) <- paste( 'gene', 1:nrow)
