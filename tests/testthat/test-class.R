1e01d7ec543c096945cf47d6b642aa437cd8a226
stela2502 <Stefan.Lang@med.lu.se>
2019-03-07 10:52:10 +0100

    speed up the correlation of sparse data.

--- tests/testthat/test-class.R
+++ tests/testthat/test-class.R
@@ -1,15 +1,20 @@
 context( 'Class usage')
 
 set.seed(1)
-dat = matrix(round(rnorm(9000,mean = 1, sd = 1)),ncol=100)
+ncol = 100
+nrow=90
+dat = matrix(round(rnorm(ncol*nrow,mean = 0, sd = 1)),ncol=ncol)
 dat[which(dat < 1)] = 0
-colnames(dat) <- paste('Sample', 1:100)
-rownames(dat) <- paste( 'gene', 1:90)
+colnames(dat) <- paste('Sample', 1:ncol)
+rownames(dat) <- paste( 'gene', 1:nrow)
 
 x <- as_FastWilcoxTest( dat )
 
+rm(dat)
+
 expect_equal( class( x )[1], c('FastWilcoxTest') )
 
+context( 'wilcox tests')
 A = 1:6
 B = 2:8
 E = log( mean(A) / mean(B) )
@@ -19,23 +24,42 @@ b= log(B+1)
 
 expect_equal( logFC ( a, b ), E,  tolerance=1e-1)
 
-
-
 Cpp = StatTest(Matrix::t(x@dat), 1:20, 30:80, .1, .1) 
 
-R = Rstats(Matrix::t(x@dat), 1:20, 30:80, .1, .1) 
+R = Rstats(Matrix::t(x@dat), 1:20, 30:80, .099, .099) 
 ## remove one value that has a p value difference of 5.372936e-03 (1.00000000 vs 0.99462706)
-Cpp = Cpp[-36,]
-R = R[-36,]
+Cpp = Cpp[-59,]
+R = R[-59,]
 expect_equal( as.vector(R[,'p.value'] ), as.vector( Cpp[,'p.value']) , tolerance=1e-10)
 
 
-Cpp = StatTest(Matrix::t(x@dat), 1:20, 30:80, .1, .1, TRUE) 
+system.time({Cpp = StatTest(Matrix::t(x@dat), 1:20, 30:80, .1, .1, TRUE) })
 
-R = Rstats(Matrix::t(x@dat), 1:20, 30:80, .1, .1, TRUE) 
+system.time({R = Rstats(Matrix::t(x@dat), 1:20, 30:80, .099, .099, TRUE) }) 
 
 Cpp = Cpp[-22,]
 R = R[-22,]
 
 expect_equal( as.vector(R[,'p.value'] ), as.vector( Cpp[,'p.value']) , tolerance=1e-10)
+
+context( 'fast cor')
+
+set.seed(1)
+
+t1 = rnorm( 30 )
+
+cor = correlationCoefficient( X= t1, Y = t1 )
+
+expect_equal( cor ,1 )
+
+cor = correlationCoefficient( X= t1, Y =rev( t1 ))
+expect_equal( cor , -0.010188, tolerance=1e-5 )
+
+system.time({ cor = CorMatrix (x@dat, x@dat[1,] ) })
+
+system.time({cor2 = apply(as.matrix(x@dat),1, cor, x@dat[1,] ) })
+
+
+all.equal(cor, as.vector(cor2), 1e-7)
+
 
