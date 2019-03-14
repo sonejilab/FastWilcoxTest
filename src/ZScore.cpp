b066f8ef246c0f3af5e9d3d736f5028590f1ac74
Stefan Lang <Stefan.Lang@med.lu.se>
2019-03-14 09:59:13 +0100

    ZScore sparse matrix exported. For use in BioData

--- src/ZScore.cpp
+++ src/ZScore.cpp
@@ -7,6 +7,17 @@ using namespace Rcpp;
 typedef Eigen::SparseVector<double> SpVec;
 typedef SpVec::InnerIterator InIterVec;
 
+
+//' @name ZScore
+//' @aliases ZScore,FastWilcoxTest-method
+//' @rdname ZScore-methods
+//' @docType methods
+//' @description A specific z. score method that converts the data to 10 +-1 instead of 0+-1
+//' in order to keep the not expressed clearly separate from the real data.
+//' @param data the sparse Matrix
+//' @param display_progress show a progress bar (TRUE)
+//' @title Calculate z score for a sparse matrix
+//' @export
 //[[Rcpp::export]]
 Eigen::SparseMatrix<double> ZScore (Eigen::SparseMatrix<double> data, bool display_progress=true){
 	/* Progress p(data.outerSize(), display_progress); */
@@ -16,21 +27,27 @@ Eigen::SparseMatrix<double> ZScore (Eigen::SparseMatrix<double> data, bool displ
 		double sum = 0.0;
 		int c = 0;
 		for (Eigen::SparseMatrix<double>::InnerIterator it(data, k); it; ++it){
-			c++;
-			sum += it.value();
+			if ( it.value() > 0) {
+				c++;
+				sum += it.value();
+			}
 		}
 		double mean = sum / c;
 		sum = 0.0;
 		for (Eigen::SparseMatrix<double>::InnerIterator it(data, k); it; ++it){
-			double entry = (it.value() - mean);
-			sum += entry * entry;
-			it.valueRef() = entry;
+			if ( it.value() > 0) {
+				double entry = (it.value() - mean);
+				sum += entry * entry;
+				it.valueRef() = entry;
+			}
 		}
 		double sd = sqrt(sum/c);
 		/*Rcout << k << " mean " << mean << " and sd " << sd << "with count "<< c<< std::endl;*/
 		for (Eigen::SparseMatrix<double>::InnerIterator it(data, k); it; ++it){
-			double entry = (it.value() / sd) +13.0 ;
-			it.valueRef() = entry;
+			if ( it.value() > 0) {
+				double entry = (it.value() / sd) +10.0 ;
+				it.valueRef() = entry;
+			}
 		}
 	}
 	data = data.transpose();
