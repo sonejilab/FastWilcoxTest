da2bc2adc1733b33041ad7aa3c648de196955d41
Stefan Lang <Stefan.Lang@med.lu.se>
2019-03-20 13:21:07 +0100

    Final commit before first release.

--- R/as_RcppTestArea.R
+++ R/as_RcppTestArea.R
@@ -1,17 +1,18 @@
-#' @name as_FastWilcoxTest
-#' @aliases as_FastWilcoxTest,FastWilcoxTest-method
-#' @rdname as_FastWilcoxTest
-#' @docType methods
-#' @description Creates a FastWilcoxTest from a matrix.
-#' @param x a matrix object
-#' @title description of function as_FastWilcoxTest
-#' @export 
+
 if ( ! isGeneric('as_FastWilcoxTest') ){setGeneric('as_FastWilcoxTest', ## Name
 	function ( x ) { 
 		standardGeneric('as_FastWilcoxTest')
 	}
 ) }
 
+#' @name as_FastWilcoxTest
+#' @aliases as_FastWilcoxTest,matrix-method
+#' @rdname as_FastWilcoxTest
+#' @docType methods
+#' @description Creates a FastWilcoxTest from a matrix.
+#' @param x a matrix object
+#' @title description of function as_FastWilcoxTest
+#' @export 
 setMethod('as_FastWilcoxTest', signature = c ('matrix'),
 	definition = function ( x ) {
 	methods::new('FastWilcoxTest', dat=Matrix::Matrix( x, sparse=T) )
--- R/meltSparseMatrix.R
+++ R/meltSparseMatrix.R
@@ -1,19 +1,16 @@
-#' @name meltSparseMatrix
-#' @aliases meltSparseMatrix,FastWilcoxTest-method
-#' @rdname meltSparseMatrix-methods
-#' @docType methods
-#' @description melt a sparse matrix into row ids, col ids and value
-#' @param x a sparse matrix
-#' @title description of function meltSparseMatrix
-#' @return a data.frame with the columns gene_id, cell_id and value
-#' containing the row ids, the col ids and the value stored in the sparse matrix using c++
-#' @export 
 if ( ! isGeneric('meltSparseMatrix') ){setGeneric('meltSparseMatrix', ## Name
 	function ( x ) { 
 		standardGeneric('meltSparseMatrix')
 	}
 ) }
-
+#' @name meltSparseMatrix
+#' @aliases meltSparseMatrix,dgCMatrix-method
+#' @rdname meltSparseMatrix-methods
+#' @docType methods
+#' @description melt a sparse matrix into a data.frame with gene.id = row ids, cell_id = col ids and value = x@x
+#' @param x a sparse matrix
+#' @title fast melt a sparse matrix omitting all '0' entries.
+#' @export 
 setMethod('meltSparseMatrix', signature = c ('dgCMatrix'),
 	definition = function ( x ) {
 	data.frame(
