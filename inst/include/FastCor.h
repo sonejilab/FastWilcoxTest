355e5db3326b1d6bf02f88a86171fcbb2d09ca9b
stela2502 <Stefan.Lang@med.lu.se>
2019-05-28 13:13:52 +0200

    merged

---
+++ inst/include/FastCor.h
@@ -0,0 +1,5 @@
+
+
+float correlationCoefficient( std::vector<double> X,  std::vector<double> Y);
+
+std::vector<double> CorMatrixIDS (Eigen::MappedSparseMatrix<double> X, std::vector<double> CMP, std::vector<int> ids ) ;
