45f0216affa2a2cd4e9a8c84f7f7e20bd4a36e53
Stefan Lang <Stefan.Lang@med.lu.se>
2019-11-06 12:24:55 +0100

    show fixed

--- R/show.R
+++ R/show.R
@@ -17,12 +17,8 @@ setGeneric('show', ## Name
 setMethod('show', signature = c ('FastWilcoxTest'),
 	definition = function (object) {
 	cat (paste("An object of class", class(object)),"\n" )
-	cat("named ",object$name,"\n")
-	cat (paste( 'with',nrow(object$dat),'genes and', ncol(object$dat),' samples.'),"\n")
-	cat (paste("Annotation datasets (",paste(dim(object$annotation),collapse=','),"): '",paste( colnames(object$annotation ), collapse="', '"),"'  ",sep='' ),"\n")
-	cat (paste("Sample annotation (",paste(dim(object$samples),collapse=','),"): '",paste( colnames(object$samples ), collapse="', '"),"'  ",sep='' ),"\n")
-	if ( length(names(object$stats)) > 0 ){
-		cat ( "P values were calculated for ", length(names(object$stats)) -1, " condition(s)\n")
-	}
+
+	cat (paste( 'with',nrow(object@dat),'genes and', ncol(object@dat),' samples.'),"\n")
+	invisible(NULL)
 })
 
