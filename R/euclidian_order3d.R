f0ad2e63b9cea99821edf4bb9dea6a1eb2916ec1
Stefan Lang <Stefan.Lang@med.lu.se>
2019-11-07 13:40:08 +0100

    Time features work more reliable

--- R/euclidian_order3d.R
+++ R/euclidian_order3d.R
@@ -10,7 +10,7 @@
 #' @title description of function euclidian_order3d
 #' @export 
 setGeneric('euclidian_order3d', ## Name
-	function ( x, y, z, last=FALSE ) { ## Argumente der generischen Funktion
+	function ( x, y, z,  last=FALSE ) { ## Argumente der generischen Funktion
 		standardGeneric('euclidian_order3d') ## der Aufruf von standardGeneric sorgt für das Dispatching
 	}
 )
@@ -18,26 +18,33 @@ setGeneric('euclidian_order3d', ## Name
 setMethod('euclidian_order3d', signature = c ('numeric'),
 	definition = function ( x, y, z, last=FALSE ) {
 
-	o = eDist3d( x, y, z ,0)
 	order = vector('numeric', length(x))
+	t = vector('numeric', length(x))
+	o = eDist3d( x, y, z ,0, t)
+	
 	order[1] = order(o)[length(o)]
 	origIDs = 1:length(o)
 	for( i in 2:length(x)) { 
 		OK = origIDs
 		a = i-1
-		if ( i >3 ) {
+		if ( i >2 ) {
 			OK = origIDs[ - order[1:(i-2)] ]
 		}
 		if ( length(OK) == 1) {
 			order[i] = origIDs[OK]
-			}else {
+		}
+		else {
 			if ( is.na( match( order[a], OK ) ) ) {
-				## OK this was a bad starting cell - re-sample that
+				message('OK this was a bad starting cell - re-sample that')
 				change = sample( 1:length(x)) 
-				ot =  euclidian_order3d( x[change], y[change], z[change] , TRUE)
+				if ( last ) {
+					browser()
+				}else {
+					ot =  euclidian_order3d( x[change], y[change], z[change] , TRUE)
+				}
 				return( change[ot] )
 			}
-			t =order( eDist3d( x[OK], y[OK], z[OK] , match( order[a], OK ) -1 ) )
+			t =order( eDist3d( x[OK], y[OK], z[OK] , match( order[a], OK ) -1, t ) )
 			order[i] = OK[t[2]]
 		}
 	}
