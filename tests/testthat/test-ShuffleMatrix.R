3d94c45333b8cb3e40b5a94f7999a8fd19aedf71
Stefan Lang <Stefan.Lang@med.lu.se>
2020-02-04 13:53:27 +0100

    Added test stub for ShuffleMatrix.

---
+++ tests/testthat/test-ShuffleMatrix.R
@@ -0,0 +1,13 @@
+set.seed(1)
+ncol = 100
+nrow=90
+dat = matrix(round(rnorm(ncol*nrow,mean = 0, sd = 1)),ncol=ncol)
+dat[which(dat < 1)] = 0
+colnames(dat) <- paste('Sample', 1:ncol)
+rownames(dat) <- paste( 'gene', 1:nrow)
+
+x <- as_FastWilcoxTest( dat )
+
+rm(dat)
+
+ShuffleMatrix( x@dat)[1:10,1:10]
