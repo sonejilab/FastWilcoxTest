2364b34f019795abdcc1d1d914a2c1f00ba33389
Stefan Lang <Stefan.Lang@med.lu.se>
2021-01-19 11:38:55 +0100

    at last the collapse function does work.

--- src/collapse.cpp
+++ src/collapse.cpp
@@ -13,18 +13,22 @@ using namespace Rcpp;
 //' @description sums up the values for each ids type
 //' @param X the sparse matrix
 //' @param ids group ids (int vector from 1 10 maxgroup for each column)
-//' @param type ( 0: logAdd (defunct); 1 : simple addition; 2: log Add -1 (defunct) )
+//' @param type ( 0: logAdd (defunct); 1 : simple addition; 2: mean )
 //' @title Calculate correlation over two double vectors
 //' @export
 // [[Rcpp::export]]
 NumericMatrix collapse (Eigen::SparseMatrix<double> X, std::vector<int> ids, int type ) {
 	int total = 0;
+	std::vector<int> USE( X.cols() );
+
 	if ( ids.size() != X.cols() )
 		::Rf_error("the ids vector needs to have the same length as the matrix columns ", type);
 	for ( int i = 0; i < ids.size(); i++ ){
 		if ( total < ids[i])
 			total = ids[i];
 	}
+	ids = FastWilcoxTest::minusOne(ids);
+
 	Rcout << "merge a matrix of " << X.rows() << " rows and "<<  X.cols() <<" into " << total << " return columns";
 	switch(type) {
 		case 0 :
@@ -35,43 +39,56 @@ NumericMatrix collapse (Eigen::SparseMatrix<double> X, std::vector<int> ids, int
 			Rcout << " using simple add"  << std::endl;
 			break;
 		case 2 :
-			Rcout << " using std::expm1"  << std::endl;
-			::Rf_error("Sorry that is not implemented correctly" );
+			Rcout << " mean"  << std::endl;
+			std::fill( USE.begin(), USE.end(), 0 ) ;
+			for ( int i = 0; i < ids.size(); i++ ){
+				USE.at(ids[i]) ++;
+			}
 			break;
 		default: ::Rf_error("Sorry the merge function type %d is not defined ", type);
 	}
 	X= X.transpose();
 
-	ids = FastWilcoxTest::minusOne(ids);
 	NumericMatrix res(X.cols(), total );
 	Rcout << " I create a results table with "  <<  X.cols() << "rows and " << total << " columns" << std::endl;
-	std::fill( res.begin(), res.end(), 0 ) ;
+	std::fill( res.begin(), res.end(), 0.0 ) ;
 	int tmp;
 
 	for (int c_=0; c_ < X.outerSize(); ++c_){
 		for (Eigen::SparseMatrix<double>::InnerIterator it(X, c_); it; ++it){
 			switch(type) {
 			case 0 :
-
 				res( c_, ids[it.row()]) += exp(it.value());
 				break;
 			case 1 :
 				res( c_, ids[it.row()]) += it.value();
 				break;
 			case 2 :
-				res( c_, ids[it.row()]) += std::expm1(it.value());
+				res( c_, ids[it.row()]) += it.value();
 				break;
 			}
 		}
-		switch(type) {
-			case 0 :
-				for (tmp = 0; tmp < total; tmp ++ ) { res( c_, tmp  ) = log( res( c_, tmp +1 ) );}
-				break;
-			case 2 :
-				for (tmp = 0; tmp < total; tmp ++ ) { res( c_, tmp  ) = log( res( c_, tmp +1 ) );}
-				break;
-		}
 	}
+	int nrows = res.nrow();
+	int ncolumns = res.ncol();
+	switch(type) {
+		case 0 :
+			for (int c_=0; c_ < ncolumns; c_++){
+				for (int i = 0; i < nrows; i++){
+					res( i, c_) = log( res( i, c_ ) +1 );
+				}
+			} 
+			break;
+		case 2 :
+			for (int c_=0; c_ < ncolumns; c_++){
+				for (int i = 0; i < nrows; i++){
+					res( i, c_) = res( i, c_ ) / USE[c_];
+				}
+			}
+			break;
+	}
+
+	
 
 	return res;
 }
