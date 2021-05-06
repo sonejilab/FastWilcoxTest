95fff1d42c93b94f3711ec6ea7b13ea3a59fb033
Stefan Lang <Stefan.Lang@med.lu.se>
2021-05-06 11:29:25 +0200

    Now the function is also exported

--- src/toColNums.cpp
+++ src/toColNums.cpp
@@ -1,8 +1,7 @@
 // [[Rcpp::depends(RcppEigen)]]
 #include <RcppEigen.h>
 #include <math.h>
-#include <iostream>
-#include <fstream> 
+
 
 using namespace Rcpp;
 typedef Eigen::SparseVector<double> SpVec;
@@ -28,30 +27,6 @@ std::vector<double> toColNums(Eigen::SparseMatrix<double> data) {
   return tmp;
 }
 
-//' @title sparse2SQLite_text_file creates a simple text file from the matrix contents
-//' @aliases sparse2SQLite_text_file,FastWilcoxTest-method
-//' @rdname sparse2SQLite_text_file
-//' @description circumvent the memory expenses during RSQlite database creation (melting the whole matrix)
-//' @param data a sparse matrix
-//' @return a vector with nGene information
-//' @export
-// [[Rcpp::export]]
-void sparse2SQLite_text_file( Eigen::SparseMatrix<double> data, String file, char sep=' '){
-  std::ofstream o(file);
-
-  for ( int k=0; k < data.outerSize(); ++k){
-
-    for (Eigen::SparseMatrix<double>::InnerIterator it(data, k); it; ++it){
-      /*
-      gene_id= x@i +1, 
-      cell_id = toColNums( x ), 
-      value= x@x
-      */
-      o << it.row() + 1 << sep << it.col() + 1 << sep << it.value() << std::endl;
-    }
-  }
-  o.close();
-}
 
 //' @title ColNotZero returns the amount of not zero values in each column
 //' @aliases ColNotZero,FastWilcoxTest-method
