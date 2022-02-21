1fc0dadb366f53b85a2dca16d7dcb74a8a926884
Stefan Lang <Stefan.Lang@med.lu.se>
2022-02-21 12:05:00 +0100

    Shuffle sparse matrix does not fail any more.

--- src/ShuffleMatrix.cpp
+++ src/ShuffleMatrix.cpp
@@ -6,6 +6,8 @@
 
 using namespace Rcpp;
 
+typedef Eigen::Triplet<double> T;
+
 //' @title reshuffle data based on a sparse matrix assuming max double the amount of entries not being zero
 //' @aliases ShuffleMatrix,FastWilcoxTest-method
 //' @rdname ShuffleMatrix
@@ -17,47 +19,78 @@ using namespace Rcpp;
 // [[Rcpp::export]]
  Eigen::SparseMatrix<double> ShuffleMatrix (Eigen::SparseMatrix<double> X, int maxCols = 50) {
 
-	Eigen::SparseMatrix<double> mat(X.innerSize(), maxCols );
-
-	int S= X.innerSize() * maxCols;
-	//Rcout << "reserving " <<  S << " entries" << std::endl;
-
-	mat.reserve( S );
+ 	std::vector<T> trp;
+ 	int max = X.nonZeros();
+ 	int maxNewGene = 0;
+ 	trp.reserve( X.nonZeros() );
+ 	if ( X.outerSize() < maxCols){
+ 		maxCols = X.outerSize();
+ 	}
 
 	//std::vector<double> A(X.innerSize());
 	NumericVector A(X.innerSize());
+	std::vector<bool> geneTaken(X.innerSize());
+
 	//NumericVector Resampled(1);
-	int Resampled = 0;
+	int randGene = 0;
 	int prob =1;
 	int i;
-	int inserted = 0;
+	int trpID = 0;
+	int tmp = 0;
+	int goodA = 0;
+	//Rcout << "create random triplets n=" << max << std::endl;
+
 	for (int c_=0; c_ < maxCols; ++c_){
-		//std::fill(A.begin(), A.end(), 0.0);
-		A.fill(0.0);
+		std::fill(A.begin(), A.end(), 0.0);
+		std::fill(geneTaken.begin(), geneTaken.end(), false);
+
 		i = 0;
 		for (Eigen::SparseMatrix<double>::InnerIterator it(X, c_); it; ++it){
 			A[i++] =  it.value();
+			//Rcout << "old value " <<  A[i-1] << std::endl;
 		}
+		goodA= i;
 		//Rcout << "calculate randoms " <<  A.length() << std::endl;
 		//NumericVector ret = RcppArmadillo::sample(x, size, replace, prob);
 		// and now we need to fill that into the sparse matrix..
 		
-		for ( int i=0; i < A.length(); i++){
-			Resampled =  std::floor(R::runif(0,1) * A.length()) ; // Rcpp::sugar::SampleReplace(A, 1, R::runif(0,A.length()), false);
-			if ( A[Resampled] != 0.0) {
-				//Rcout << "insert " <<  i << ","<< c_ <<" value " << A[Resampled] << std::endl;
-				mat.insert(i, c_ ) = A[Resampled];
-				inserted ++;
+		for ( int i=0; i < goodA; i++){
+			if ( A[i] == 0.0 || A[i] == 0){
+				//Rcout << "col broken at A["<<i<<"]="<<A[i]<< " and running id= " <<  trpID << std::endl;
+				break;
+			}
+
+			randGene = std::floor(R::runif(0,1) * A.length()) ;
+
+			while ( geneTaken[randGene] ){
+				randGene = std::floor(R::runif(0,1) * A.length()) ;
+			}
+			geneTaken[randGene] = true;
+
+			if ( i == 0 ){
+				//Rcout << "inst " <<  randGene << ","<< c_ <<" value " << A[i] << std::endl;
+				tmp = trpID;
+			}
+			
+			if ( maxNewGene < randGene){
+				maxNewGene = randGene ;
+			}
+			if ( trpID > max ){
+				//Rcout << "triplets exceed max counts - stop" << std::endl;
+			}else {
+				trp.push_back(T(randGene, c_, A[i]) ) ;
+				trpID++;
 			}
 		}
+		//Rcout << "max geneID = "<< maxNewGene << std::endl;
+		//Rcout << "do we have access to the triplets? t[" << tmp << "] = " << trp[tmp].row() <<
+		//		", " <<  trp[tmp].col() << ", "<< trp[tmp].value() << std::endl;
 	}
+
+	//Rcout << "inserted a total of " << trpID << " values into the triplet list"<< std::endl;
 	Eigen::SparseMatrix<double> mat2(X.innerSize(), maxCols );
-	mat2.reserve( inserted );
-	for (int c_=0; c_ < maxCols; ++c_){
-		for (Eigen::SparseMatrix<double>::InnerIterator it(mat, c_); it; ++it){
-			mat2.coeffRef(it.row(), it.col() ) = it.value();
-		}
-	}
+
+	mat2.setFromTriplets(trp.begin(), trp.end());
 	//Rcout << "inserted a total of " << inserted << " values into the dgCMatrix"<< std::endl;
 	return ( mat2 );
 }
--- tests/testthat/test-ShuffleMatrix.R
+++ tests/testthat/test-ShuffleMatrix.R
@@ -7,14 +7,21 @@ colnames(dat) <- paste('Sample', 1:ncol)
 rownames(dat) <- paste( 'gene', 1:nrow)
 
 x <- as_FastWilcoxTest( dat )
-
+xv = apply( x@dat[,1:50], 2, var )
+xn = FastWilcoxTest::ColNotZero( x@dat[,1:50])
 rm(dat)
 
 #warning("Still OK 1")
 d1 = ShuffleMatrix( x@dat)
 
-#warning("Still OK 2")
+expect_equal( xn, FastWilcoxTest::ColNotZero( d1) ,label="same density first 50 cols" )
+
+tr1 =apply( d1, 2, var )
+expect_equal( as.vector(xv), tr1 ,label="same variance (cells) first 50 cols" )
+
 t1 =apply( d1, 1, var )
+expect_true( ! all (apply( x@dat[,1:50], 1, var ) ==  t1) ,label="different variance (cells) first 50 cols" )
+
 #Sys.sleep(1)
 #warning("Still OK 3")
 d2 = ShuffleMatrix( x@dat)
@@ -23,3 +30,15 @@ d2 = ShuffleMatrix( x@dat)
 t2 =apply( d2, 1, var )
 
 expect_true( ! all ( t2 == t1) ,label="creating different results every time" )
+
+prefix = "."
+path = file.path( prefix, 'data', 'problematic.RData' )
+
+
+load( path )
+dat = Matrix::drop0( x@dat)
+d3 = ShuffleMatrix( dat)
+
+tr3 =apply( d3, 2, var )
+expect_equal( as.vector(apply(dat,2, var)), tr3 ,label="same variance (cells) first 50 cols" )
+
